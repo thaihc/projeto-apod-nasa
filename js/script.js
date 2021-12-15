a91b89fe35ca4adf3fed01ae236cffbfa998dd1b
thaihc <thaisherculanodecarvalho@gmail.com>
2021-12-15 13:05:35 -0300

    complementei o javascript com informações adicionais da API

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 let but = $("#search");
+let text = $("#text");
+let author = $("#author");
+let images = $("#image");
+let video = $("#video");
+let title = $("#title");
+
 but.on("click", function () {
   let date = $("#date-select").val();
   $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=zmtwe6hEGZoepuVy2rub7QyPzdx3R4YU2XvAMVTi&date=${date}`,
     type: "GET",
     contentType: "application/json",
+
     success: function (response) {
-      $("#text").html(`${response.explanation}`);
-      $("#author").html(`By: ${response.copyright}`);
-      $("#image").attr("src", response.url);
+      title.html(`${response.title}`);
+      text.html(`${response.explanation}`);
+      author.html(`By: ${response.copyright}`);
+      if (response.media_type == "image") {
+        images.attr("src", response.url);
+      } else {
+        document.getElementById("image").style.display = "none";
+        document.getElementById("video").style.display = "block";
+        video.attr("src", response.url);
+      }
+
       return response;
     },
   });
