9a10e8dc509cff87e0015a1333cef17b7aa2ea3e
thaihc <thaisherculanodecarvalho@gmail.com>
2021-12-15 11:57:51 -0300

    organizei as pastas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let but = $("#search");
 but.on("click", function () {
+  let date = $("#date-select").val();
   $.ajax({
-    url: `https://api.nasa.gov/planetary/apod?api_key=zmtwe6hEGZoepuVy2rub7QyPzdx3R4YU2XvAMVTi&date=${date.value}`,
+    url: `https://api.nasa.gov/planetary/apod?api_key=zmtwe6hEGZoepuVy2rub7QyPzdx3R4YU2XvAMVTi&date=${date}`,
+    type: "GET",
+    contentType: "application/json",
     success: function (response) {
       $("#text").html(`${response.explanation}`);
       $("#author").html(`By: ${response.copyright}`);
