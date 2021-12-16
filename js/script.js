9db5ce4c01c6bc61f44454cbd080a761c0ae3315
thaihc <thaisherculanodecarvalho@gmail.com>
2021-12-15 23:23:30 -0300

    ajustes no javascript para o campo data

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
+//Criação de variáveis para fixar a data atual no input date.
+
+let today = new Date();
+let day = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+
+//Criação de variáveis que serão utilizadas na função request.
+
 let but = $("#search");
+let video = $("#video");
 let title = $("#title");
 let chosenDate = $("#date-init");
 let text = $("#text");
 let author = $("#author");
-let images = $("#image");
-let video = $("#video");
+let images = $("#images");
+
+//Evento para fixar o conteúdo correspondente a data atual ao carregar/recarregar a página inicial.
+
+window.onload = function () {
+  document.getElementById("date-select").value = year + "-" + month + "-" + day;
+  request();
+};
+
+// Evento para acionar a função request ao clicar no botão.
 
 but.on("click", function () {
+  request();
+});
+
+//Função que retorna os dados solicitados na API.
+
+function request() {
   let date = $("#date-select").val();
   $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=zmtwe6hEGZoepuVy2rub7QyPzdx3R4YU2XvAMVTi&date=${date}`,
@@ -20,12 +44,14 @@ but.on("click", function () {
       author.html(`By: ${response.copyright}`);
       if (response.media_type == "image") {
         images.attr("src", response.url);
+        document.getElementById("images").style.display = "block";
+        document.getElementById("video").style.display = "none";
       } else {
-        document.getElementById("image").style.display = "none";
+        document.getElementById("images").style.display = "none";
         document.getElementById("video").style.display = "block";
         video.attr("src", response.url);
       }
       return response;
     },
   });
-});
+}
